a587c081a1270e894537f590aa8b1da524fe5a7c
yogesh2019 <yogeshkkhichi@gmail.com>
2025-08-15 15:10:08 +0530

    write side - writing

---
+++ ERP_V5_Application/Common/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ERP_V5_Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_V5_Application.Common.Interfaces;
+public interface IProductRepository
+{
+    Task AddAsync(Product product, CancellationToken cancellationToken = default);
+    Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken = default);
+}
---
+++ ERP_V5_Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_V5_Application.Common.Interfaces;
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
