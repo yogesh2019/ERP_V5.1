c0f59c6a591cb186654704340f3f27a6b52d50dd
yogesh2019 <yogeshkkhichi@gmail.com>
2026-01-29 15:53:42 +0530

    adding sidebar component

--- erp-v5-ui/src/app/layout/main-layout/main-layout.component.ts
+++ erp-v5-ui/src/app/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { SideBarComponent } from "../sidebar/sidebar.component";
 
 @Component(
     {
         selector: 'app-main-layout',
         standalone: true,
-        imports: [RouterOutlet],
+        imports: [RouterOutlet, SideBarComponent],
         templateUrl: './main-layout.component.html'
     }
 )
