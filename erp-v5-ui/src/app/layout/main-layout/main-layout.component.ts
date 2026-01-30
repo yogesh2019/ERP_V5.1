271d62d010b1dcd38a8204bb391fbb960c041c4b
yogesh2019 <yogeshkkhichi@gmail.com>
2026-01-30 14:21:26 +0530

    adding main layout

---
+++ erp-v5-ui/src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { AppRoutingModule } from "src/app/app-routing.module";
+
+@Component({
+    selector: 'app-main-layout',
+    templateUrl: './main-layout.component.html'
+})
+export class MainLayoutComponent {
+
+}
